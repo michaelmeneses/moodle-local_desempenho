5c553185bae42e9745034774119e31656e2eb5e3
Michael Meneses <michael.douglas.meneses.2@gmail.com>
2017-04-04 18:34:12 -0300

    Build version 1.0

--- version.php
+++ version.php
@@ -8,8 +8,8 @@
  * @license   UNIPRE {@link http://www.cursounipre.com.br}
  */
 
-$plugin->version = 2017032700;   // The (date) version of this plugin
+$plugin->version = 2017040400;   // The (date) version of this plugin
 $plugin->requires = 2016112900; // Requires this Moodle version
 $plugin->component = 'local_desempenho';  // To check on upgrade, that module sits in correct place
-$plugin->release = "0.1"; // Human readable version name
+$plugin->release = "1.0"; // Human readable version name
 $plugin->maturity = MATURITY_ALPHA; //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
